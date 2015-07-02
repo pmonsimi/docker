89c19cfac09f172b300d999935901dfa39c50c4b
Pierre Brun <pbrun.pro@gmail.com>
2015-07-03 01:54:59 +0200

    [A] First try to create a docker image for framadate

---
+++ framadate/scripts/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /var/www
+
+mv /var/www/app/inc/constants.php.template /var/www/app/inc/constants.php
+sed -i -e"s/<database type>/mysql/" /var/www/app/inc/constants.php
+sed -i -e"s/<www.mydomain.com>/$APP_DOMAINE/" /var/www/app/inc/constants.php
+sed -i -e"s/<Application name>/$APP_NAME/" /var/www/app/inc/constants.php
+sed -i -e"s/<email address>/$APP_EMAIL/" /var/www/app/inc/constants.php
+sed -i -e"s/<no-reply@mydomain.com>/$APP_EMAIL_NO_REPLY/" /var/www/app/inc/constants.php
+sed -i -e"s/<database name>/$MYSQL_NAME/" /var/www/app/inc/constants.php
+sed -i -e"s/<database user>/$MYSQL_USER/" /var/www/app/inc/constants.php
+sed -i -e"s/<database password>/$MYSQL_PASSWORD/" /var/www/app/inc/constants.php
+sed -i -e"s/<database server>/MYSQL_PORT_3306_TCP_ADDR:MYSQL_PORT_3306_TCP_PORT/" /var/www/app/inc/constants.php
+sed -i -e"s/<relative path to the logo file>/ /" /var/www/app/inc/constants.php
+sed -i -e"s/<relative path to the logo file for pdf>/ /" /var/www/app/inc/constants.php
+
+php -r "readfile('https://getcomposer.org/installer');" | php
+./composer.phar install --no-interaction
+
+service apache2 restart
